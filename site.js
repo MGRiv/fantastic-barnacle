3a0c20965eaf775bd6b3238d583cb5a65c8131c4
Chris Grant <chris@chrisgrant.co>
2016-03-28 13:19:39 -0400

    change

---
+++ site.js
@@ -0,0 +1,3 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoiY2hyaXNnY28iLCJhIjoiY2lnODIxamRlMDlmYXRmbTY3YmRrbGZmbSJ9.7cteu78r4b6UYlHUaYvxTA';
+var map = L.mapbox.map('map', 'mapbox.streets')
+  .setView([37.09024, -95.712891], 5);
